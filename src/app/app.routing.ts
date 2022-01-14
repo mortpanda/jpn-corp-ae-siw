155065ecba31e86c6895289c3e276d512907f147
Kent Nagao <31921593+mortpanda@users.noreply.github.com>
2022-01-14 17:56:57 +0900

    main
    main

---
+++ src/app/app.routing.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+import {LandingComponent} from 'app/landing/landing.component';
+// import {SamplePageComponent} from 'app/sample-page/sample-page.component'
+import {StartComponent} from 'app/start/start.component';
+
+
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: LandingComponent },
+  { path: 'start', component: StartComponent },
+  // { path: 'sample', component: SamplePageComponent },
+  
+  // { path: 'profilev2', component: Profilev2Component },
+  // { path: 'store', component: SwagstoreComponent },
+  // { path: 'checkout', component: CheckoutComponent },
+  // { path: 'save', component: SaveModalComponent },
+  //{ path: 'signup',           component: SignupComponent },
+  //{ path: 'landing',          component: LandingComponent },
+  // { path: 'nucleoicons',      component: NucleoiconsComponent }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes, {
+      useHash: false
+    })
+  ],
+  exports: [
+  ],
+})
+export class AppRoutingModule { }
